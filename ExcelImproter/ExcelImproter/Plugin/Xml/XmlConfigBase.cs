1912d654658740f4004830a553b17b48909ebd6d
baoxue <Blizzard_xc@126.com>
2016-08-02 13:16:24 +0800

    update xml serializer

--- ExcelImproter/ExcelImproter/Plugin/Xml/XmlConfigBase.cs
+++ ExcelImproter/ExcelImproter/Plugin/Xml/XmlConfigBase.cs
@@ -1,41 +1,42 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Text;
 using System.Xml.Serialization;
-using System.Collections;
 using System.Xml;
 
-public abstract class XmlConfigBase 
+namespace Common.Config
 {
-    public static string Serialize<T>(T config) where T : XmlConfigBase, new()
+    public abstract class XmlConfigBase
     {
-        XmlSerializer serializer = new XmlSerializer(typeof(T));
-        
-        using (Stream outputStream = new MemoryStream(64))
+        public static string Serialize<T>(T config) where T : XmlConfigBase, new()
         {
-            XmlTextWriter a = new XmlTextWriter(outputStream, Encoding.UTF8);
-            a.Formatting = Formatting.Indented;
-            serializer.Serialize(a, config);
-            byte[] bytes = new byte[outputStream.Length];
-            outputStream.Position = 0;
-            outputStream.Read(bytes, 0, bytes.Length);
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
 
+            using (Stream outputStream = new MemoryStream(64))
+            {
+                XmlTextWriter a = new XmlTextWriter(outputStream, Encoding.UTF8);
+                a.Formatting = Formatting.Indented;
+                serializer.Serialize(a, config);
+                byte[] bytes = new byte[outputStream.Length];
+                outputStream.Position = 0;
+                outputStream.Read(bytes, 0, bytes.Length);
+
+                Encoding encoding = Encoding.UTF8;
+                string res = encoding.GetString(bytes);
+                return res;
+            }
+        }
+        public static T DeSerialize<T>(string xmlData) where T : XmlConfigBase, new()
+        {
             Encoding encoding = Encoding.UTF8;
-            string res = encoding.GetString(bytes);
-            return res;
-        }
-    }
-    public static T DeSerialize<T>(string xmlData) where T : XmlConfigBase, new()
-    {
-        Encoding encoding = Encoding.UTF8;
-        byte[] data = encoding.GetBytes(xmlData);
-        XmlSerializer serializer = new XmlSerializer(typeof(T));
+            byte[] data = encoding.GetBytes(xmlData);
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
 
-        T sysConfig = null;
-        using (MemoryStream stream = new MemoryStream(data))
-        {
-            sysConfig = serializer.Deserialize(stream) as T;
+            T sysConfig = null;
+            using (MemoryStream stream = new MemoryStream(data))
+            {
+                sysConfig = serializer.Deserialize(stream) as T;
+            }
+            return sysConfig;
         }
-        return sysConfig;
     }
 }
